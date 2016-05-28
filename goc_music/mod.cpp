49a79634a0f4bee709fb1db4747601ee5d997e2d
Bruce <brucetaylor88@gmx.com>
2016-05-28 15:31:34 +0100

    First addition of assets
    Modified:
    
    Added:
    
    Various assets
    
    Removed:

---
+++ goc_music/mod.cpp
@@ -0,0 +1,14 @@
+name = "Ghosts of Chernarus Music";
+picture = "\goc_music\goc_logo_medium.paa";
+actionName = "Website";
+action = "www.yermaw.com";
+logo = "\goc_music\goc_logo.paa";
+logoOver = "\goc_music\goc_logo.paa";
+logoSmall = "\goc_music\goc_logo.paa";
+tooltip = "Ghost of Chernarus";
+tooltipOwned = "Ghosts of Chernarus";
+overview = "Reviving the Ghosts in a new combat experience in Chernarus.";
+author = "ecurb";
+overviewPicture = "\goc_music\goc_logo_medium.paa";
+overviewText = "Ghosts of Chernarus";
+overviewFootnote = "One shot one kill";
---
+++ goc_patches/config.cpp
@@ -0,0 +1,20 @@
+class CfgPatches
+{
+	class GOC_Patches
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+	};
+};
+class CfgUnitInsignia
+{
+	class GOC_Patch
+	{
+		displayName = "Ghost Patch";
+		author = "ecurb";
+		texture = "\goc_patches\goc_patch.paa";
+		textureVehicle = "";
+	};
+};
